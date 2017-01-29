73a213e762b3b4fedabb3da20549d6672ad2f3e8
Jack McCracken <jack.mccracken@shopify.com>
2017-01-28 19:44:44 -0500

    Add to Main class

--- A1/COMP4601SDA/src/edu/carleton/COMP4601/resources/Main.java
+++ A1/COMP4601SDA/src/edu/carleton/COMP4601/resources/Main.java
@@ -1,5 +1,22 @@
 package edu.carleton.COMP4601.resources;
 
-public class Main {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
 
+@Path("/sda")
+public class Main {
+	// Allows to insert contextual objects into the class,
+		// e.g. ServletContext, Request, Response, UriInfo
+	@Context
+	UriInfo uriInfo;
+	@Context
+	Request request;
+
+	@GET
+	public String printName() {
+		return "COMP4601 Searchable Document Archive";
+	}
 }
